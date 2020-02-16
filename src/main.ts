dfb8bb3a911ccb56b593a4ddc27f8c2ad1f53a59
Marcus Coleman <Sunhat@users.noreply.github.com>
2020-02-16 17:40:59 +0000

    Tidy up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import Vue from 'vue'
-import VueX from 'vuex'
-import VueColumnsResizable from 'vue-columns-resizable'
 import store from './store'
 import router from './router'
 import App from './App.vue'
@@ -8,7 +6,6 @@ import VueCompositionApi from '@vue/composition-api'
 
 Vue.config.productionTip = false
 
-Vue.use(VueColumnsResizable)
 Vue.use(VueCompositionApi)
 
 new Vue({
