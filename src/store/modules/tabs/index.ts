70b7739738d18b1a4a8fa69071f0be8532c81d36
Marcus Coleman <Sunhat@users.noreply.github.com>
2020-03-04 08:59:18 +0000

    Stop clicking tab while it's closing

--- src/store/modules/tabs/index.ts
+++ src/store/modules/tabs/index.ts
@@ -105,6 +105,8 @@ const actions = {
   },
   changeTab({ commit, state }: any, uid: string) {
     return new Promise((resolve, reject) => {
+      const tab: Tab = state.tabs.find((tab: Tab) => tab.uid === uid)
+      if (tab?.disabled) return
       if (state.selectedTabUid !== uid) {
         Router.replace({ name: 'tab-home', params: { uid: uid }})
         commit('SET_TAB', uid)
